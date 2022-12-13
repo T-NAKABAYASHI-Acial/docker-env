126d10a27aca622daa8c5468c315667e912477af
szktty <szk0115tty@gmail.com>
2022-12-13 23:29:47 +0900

    更新

--- app/routes/web.php
+++ app/routes/web.php
@@ -11,13 +11,17 @@
 |
 */
 
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
+Route::group(['middleware' => 'auth'], function () {
+    Route::get(
+        '/',
+        function () {
+            return view('welcome');
+        }
+    );
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::get('/home', 'HomeController@index')->name('home');
 });
-
-Route::get('/home', 'HomeController@index')->name('home');
